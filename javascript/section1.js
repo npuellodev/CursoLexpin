5043571c0a787197993f06ee87f8344ce48054d6
Nicolas Guerrero <nicolasguerrerop592@gmail.com>
2024-11-03 08:04:56 -0400

    javascript

---
+++ javascript/section1.js
@@ -0,0 +1,61 @@
+alert('archivo de js');
+//comentario de una linea
+/*
+  comentario de 
+  varias lineas
+*/ 
+/*
+//Console
+console.log('Hello world')
+//Variables
+var nombre = 'Nicolas';
+var apellido = 'Guerrero';
+
+//tipos de datos
+
+//numeros
+var numero = 5;
+var numero_float = 2.1;
+//strings
+var nombre = 'Nicolas';
+var apellido = 'Guerrero';
+var nombre_completo = nombre + ' ' + apellido;
+//booleanos
+var es_mayor_de_edad = true;
+//arrays 
+var arrays = [1,2,3,4,5];
+var array_variado = [1,"golazo",true,2,3,4,5];
+//objetos
+var persona = {
+  nombre: 'Nicolas',
+  apellido: 'Guerrero',
+  edad: 20
+};
+//indefinido
+var indefinido;
+//null
+var nulo = null;
+
+console.log("mi nombre es "+nombre+" "+apellido);
+*/
+
+//ejercicio de practica
+
+var num1 = 10;
+var num2 = 20;
+var num3 = 30;
+var operador = "+";
+
+if (operador == "+") {
+  console.log(num1+num2+num3);
+}else if(operador == "-"){
+  console.log(num1-num2-num3);
+}else if (operador == "*"){
+  console.log(num1*num2*num3);
+}else if (operador == "/"){
+  console.log(num1/num2/num3);
+}else if (operador == "%"){
+  console.log(num1%num2%num3);
+}else{
+  console.log("operador invalido");
+}
---
+++ javascript/section2.js
@@ -0,0 +1,75 @@
+//SCOPE (Alcance)
+//Variable global
+var nombre = 'Nicolas';
+//Variable local
+let apellido = 'Guerrero';
+//Variable constante
+const PI = 3.1416;
+
+//ejercicio de la calculadora con funciones
+/*
+var a = 10;
+var b = 20;
+operador = "+";
+function suma(a,b){
+  var sumaResult = a+b;
+  return sumaResult;
+}
+
+function resta(a,b){
+  var restaResult = a-b;
+  return restaResult;
+}
+
+function multiplicacion(a,b){
+  var multiplicacionResult = a*b;
+  return multiplicacionResult;
+}
+
+function division(a,b){
+  var divisionResult = a/b;
+  return divisionResult;
+}
+
+if(operador == "+"){
+  console.log(suma(a,b));
+}else if(operador == "-"){
+  console.log(resta(a,b));
+}else if(operador == "*"){
+  console.log(multiplicacion(a,b));
+}else if(operador == "/"){
+  console.log(division(a,b));
+}else{
+  console.log("Operador invalido");
+}
+*/
+/*
+function salir(tiempo = false, dinero = false, ganas = false){
+  if(tiempo && dinero && ganas){
+    console.log("Saldre a divertirme");
+  }if(tiempo && dinero){
+    console.log("Salir desganado");
+  }if(tiempo){
+    if(ganas){
+    console.log("saldre a hacer ejercicio");
+    }
+    return "saldre a matar el timepo"
+  }
+  return "no saldre"
+};
+
+console.log(salir(true, false, true));
+*/
+/*
+factorial con recursividad
+function factorial(n){
+  if(n>0){
+    return n*factorial(n-1);
+  }else{
+    return 1
+  }
+}
+
+console.log(factorial(5))
+*/
+
